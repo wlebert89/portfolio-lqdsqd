affb7f9c044bf56d924cdd99bb2a7c6fa648f13c
lqdsqd <wesley.lebert@gmail.com>
2019-06-18 14:45:08 -0500

    updated nav links to the correct routes

--- lqdsqd-react-app/src/components/Nav/Nav.js
+++ lqdsqd-react-app/src/components/Nav/Nav.js
@@ -9,9 +9,9 @@ function Nav() {
                     lqdsqd
             </div>
                 <div id="nav">
-                    <a className="nav-link" href="portfolio.html" target="_blank">PROJECTS</a>
-                    <a className="nav-link" href="index.html">ABOUT</a>
-                    <a className="nav-link" href="contact.html">CONTACT</a>
+                    <a className="nav-link" href="/portfolio">PROJECTS</a>
+                    <a className="nav-link" href="/about">ABOUT</a>
+                    <a className="nav-link" href="/contact">CONTACT</a>
                     <a className="nav-link fab fa-instagram" href="https://www.instagram.com/lqdsqd/" target="_blank" rel="noopener noreferrer"> </a>
                     <a className="nav-link fab fa-github-square" href="https://github.com/wlebert89" target="_blank" rel="noopener noreferrer"> </a>
                     <a className="nav-link fab fa-linkedin-in" href="https://www.linkedin.com/in/wesley-lebert-9a147a180/" target="_blank" rel="noopener noreferrer"> </a>
