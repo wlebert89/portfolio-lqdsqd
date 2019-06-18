1d8ba2cb66fc5cf0fc1845acf7c30e858c9538e8
lqdsqd <wesley.lebert@gmail.com>
2019-06-18 13:24:56 -0500

    working on Nav and figuring out why fonts aren't loading globally

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,31 @@
 import React from "react";
+import "./Nav.css";
 
 function Nav() {
     return (
-        <div>
-            Nav.
-        </div>
+        <header>
+            <div class="container">
+                <div id="logo">
+                    lqdsqd
+            </div>
+                <div id="nav">
+                    <a className="nav-link" href="portfolio.html" target="_blank">PROJECTS</a>
+                    <a className="nav-link" href="index.html">ABOUT</a>
+                    <a className="nav-link" href="contact.html">CONTACT</a>
+                    <a className="nav-link fab fa-instagram" href="https://www.instagram.com/lqdsqd/" target="_blank" rel="noopener noreferrer"> </a>
+                    <a className="nav-link fab fa-github-square" href="https://github.com/wlebert89" target="_blank" rel="noopener noreferrer"> </a>
+                    <a className="nav-link fab fa-linkedin-in" href="https://www.linkedin.com/in/wesley-lebert-9a147a180/" target="_blank" rel="noopener noreferrer"> </a>
+                </div>
+                <div id="hamburger-menu">
+                    <div id="nav-icon1">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+
+            </div>
+        </header>
     )
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Nav from "../components/Nav/Nav"
 
 function About() {
     return (
         <div>
-            About.
+            <Nav />
+            <div>
+                About.
+            </div>
         </div>
     )
 }
