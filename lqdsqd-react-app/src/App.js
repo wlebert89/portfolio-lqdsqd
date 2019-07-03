c1b6a25da03732927ef09d227c33faafb1892a7d
lqdsqd <wesley.lebert@gmail.com>
2019-07-03 14:35:46 -0500

    changed browserRouter to HashRouter

--- lqdsqd-react-app/src/App.js
+++ lqdsqd-react-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Project from "./pages/Project/Project"
