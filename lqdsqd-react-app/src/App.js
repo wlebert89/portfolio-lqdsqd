0d06a2660895933cd9dd9bacd75e3209ad6b20a6
lqdsqd <wesley.lebert@gmail.com>
2019-07-04 17:57:22 -0500

    working on responsive layout for the project page

--- lqdsqd-react-app/src/App.js
+++ lqdsqd-react-app/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About/About";
 import Portfolio from "./pages/Portfolio/Portfolio";
 import Project from "./pages/Project/Project"
-import Contact from "./pages/Contact/Contact";
 import NoMatch from "./pages/NoMatch/NoMatch";
 import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer"
@@ -18,7 +17,6 @@ function App() {
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/portfolio/project/:id" component={Project} />
-          <Route exact path="/contact" component={Contact} />
           <Route component={NoMatch} />
         </Switch>
       </div>
--- lqdsqd-react-app/src/components/Nav/Nav.js
+++ lqdsqd-react-app/src/components/Nav/Nav.js
@@ -2,27 +2,19 @@ import React from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
 
-function Nav() {    
+function Nav() {
     return (
         <header>
             <div className="container">
                 <div id="logo">
-                    <Link to="/" style={{color: "#000"}}> lqdsqd </Link>
-            </div>
+                    <Link to="/" style={{ color: "#000" }}> lqdsqd </Link>
+                </div>
                 <div id="nav">
                     <span className="nav-link"><Link to={"/portfolio"}>PROJECTS</Link></span>
-                    <span className="nav-link"><Link activeClassName="active-nav" to={"/about"}>ABOUT</Link></span>
-                    <span className="nav-link"><Link activeClassName="active-nav" to={"/contact"}>CONTACT</Link></span>
+                    <span className="nav-link"><Link to={"/about"}>ABOUT</Link></span>
                     <a className="nav-link fab fa-instagram" href="https://www.instagram.com/lqdsqd/" target="_blank" rel="noopener noreferrer"> </a>
                     <a className="nav-link fab fa-github-square" href="https://github.com/wlebert89" target="_blank" rel="noopener noreferrer"> </a>
                     <a className="nav-link fab fa-linkedin-in" href="https://www.linkedin.com/in/wesley-lebert-9a147a180/" target="_blank" rel="noopener noreferrer"> </a>
-                </div>
-                <div id="hamburger-menu">
-                    <div id="nav-icon1">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
                 </div>
             </div>
         </header>
