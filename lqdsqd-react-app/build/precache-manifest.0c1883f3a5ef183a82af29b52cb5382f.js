a9bd3b91af0c35a2c4f724a8fa7082e4ae5bb410
lqdsqd <wesley.lebert@gmail.com>
2019-08-17 12:17:51 -0500

    reverting back to position:fixed on bottom bar

--- lqdsqd-react-app/build/precache-manifest.0c1883f3a5ef183a82af29b52cb5382f.js
+++ lqdsqd-react-app/build/precache-manifest.0c1883f3a5ef183a82af29b52cb5382f.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "987947d1332b2dc2e8e5775841debcb8",
+    "revision": "9fc4682c485fecb11c66431209357bf8",
     "url": "/index.html"
   },
   {
-    "revision": "4f08b37ba4ddd7363ccc",
-    "url": "/static/css/main.05920272.chunk.css"
+    "revision": "ffa5f816ef91a61d894f",
+    "url": "/static/css/main.ecb8b18d.chunk.css"
   },
   {
     "revision": "67c9ff8585b721bf7eba",
     "url": "/static/js/2.4fe97120.chunk.js"
   },
   {
-    "revision": "4f08b37ba4ddd7363ccc",
+    "revision": "ffa5f816ef91a61d894f",
     "url": "/static/js/main.a9482e61.chunk.js"
   },
   {
