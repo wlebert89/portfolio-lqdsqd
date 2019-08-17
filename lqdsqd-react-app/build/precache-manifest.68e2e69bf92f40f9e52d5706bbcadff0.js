9f4d21f2e356f852168030868182d9c8b409a826
lqdsqd <wesley.lebert@gmail.com>
2019-08-16 23:07:01 -0500

    working on mobile scroll bug

--- lqdsqd-react-app/build/precache-manifest.68e2e69bf92f40f9e52d5706bbcadff0.js
+++ lqdsqd-react-app/build/precache-manifest.68e2e69bf92f40f9e52d5706bbcadff0.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "069496f2a33ff00f531d881365ff6a1a",
+    "revision": "28a05e3d49af42b257091c268b3e0165",
     "url": "/index.html"
   },
   {
-    "revision": "b0b11e15a8f3f1f4ef0a",
-    "url": "/static/css/main.e7175c22.chunk.css"
+    "revision": "33353e1165c2e8b84f80",
+    "url": "/static/css/main.a824d566.chunk.css"
   },
   {
     "revision": "67c9ff8585b721bf7eba",
     "url": "/static/js/2.4fe97120.chunk.js"
   },
   {
-    "revision": "b0b11e15a8f3f1f4ef0a",
+    "revision": "33353e1165c2e8b84f80",
     "url": "/static/js/main.13c8fad8.chunk.js"
   },
   {
