7bb07465e65aa727aee453d4ad312401865a9cb0
lqdsqd <wesley.lebert@gmail.com>
2019-08-18 12:36:02 -0500

    Updates

--- precache-manifest.586c37847f199d1207e428c3e16fe1c7.js
+++ precache-manifest.586c37847f199d1207e428c3e16fe1c7.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d041d5454ea7845bde3c9736b1f0a0ba",
+    "revision": "619a255d066ff81e670b9924e94bd2c1",
     "url": "/index.html"
   },
   {
-    "revision": "573ba175be86fe60d161",
-    "url": "/static/css/main.ffd34dcc.chunk.css"
+    "revision": "04dda9804ddfcf74a961",
+    "url": "/static/css/main.a8ddeb7b.chunk.css"
   },
   {
     "revision": "ed1f79e7e2f9dd3d0c98",
     "url": "/static/js/2.a64d851f.chunk.js"
   },
   {
-    "revision": "573ba175be86fe60d161",
+    "revision": "04dda9804ddfcf74a961",
     "url": "/static/js/main.4bff9503.chunk.js"
   },
   {
