1f87ad8ac153e9d8219e1349c3f09fe3a05b634c
lqdsqd <wesley.lebert@gmail.com>
2019-08-18 12:43:33 -0500

    we're back

--- lqdsqd-react-app/build/service-worker.js
+++ lqdsqd-react-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c14f739b53b37e96dd615a5e3b3a1ac.js"
+  "/precache-manifest.586c37847f199d1207e428c3e16fe1c7.js"
 );
 
 self.addEventListener('message', (event) => {
