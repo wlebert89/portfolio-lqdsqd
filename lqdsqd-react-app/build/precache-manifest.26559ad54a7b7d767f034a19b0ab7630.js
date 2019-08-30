94f54e8a304c4aad5f0df6e01ccb158ed3111720
lqdsqd <wesley.lebert@gmail.com>
2019-08-30 15:43:59 -0500

    updating color overlays

--- lqdsqd-react-app/build/precache-manifest.26559ad54a7b7d767f034a19b0ab7630.js
+++ lqdsqd-react-app/build/precache-manifest.26559ad54a7b7d767f034a19b0ab7630.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d0e60aa799e18b91d2cd4980b67ede1",
+    "revision": "9aab37a7c3ca23617f2f7a5f54ce78c4",
     "url": "/index.html"
   },
   {
-    "revision": "ee28d3bb88dc65a76dd1",
-    "url": "/static/css/main.15d0e20b.chunk.css"
+    "revision": "9f44306f805055d89d5a",
+    "url": "/static/css/main.ad3b0495.chunk.css"
   },
   {
     "revision": "ed1f79e7e2f9dd3d0c98",
     "url": "/static/js/2.a64d851f.chunk.js"
   },
   {
-    "revision": "ee28d3bb88dc65a76dd1",
+    "revision": "9f44306f805055d89d5a",
     "url": "/static/js/main.744ac804.chunk.js"
   },
   {
