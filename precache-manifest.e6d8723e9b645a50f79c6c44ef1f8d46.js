1e5c45dce041b5f29d8ccd2828d00853e74d98ed
lqdsqd <wesley.lebert@gmail.com>
2019-10-08 18:26:04 -0500

    Updates

--- precache-manifest.e6d8723e9b645a50f79c6c44ef1f8d46.js
+++ precache-manifest.e6d8723e9b645a50f79c6c44ef1f8d46.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b8e061fa3046c861dabcd912d1cc2c7c",
+    "revision": "d97782608003afd76a17a35d2e68dbd1",
     "url": "/index.html"
   },
   {
-    "revision": "5bd7a3dea0635b9c4645",
-    "url": "/static/css/main.9fe635c4.chunk.css"
+    "revision": "22df34e7e6d2af076f12",
+    "url": "/static/css/main.f9ad7a87.chunk.css"
   },
   {
     "revision": "ed1f79e7e2f9dd3d0c98",
     "url": "/static/js/2.a64d851f.chunk.js"
   },
   {
-    "revision": "5bd7a3dea0635b9c4645",
+    "revision": "22df34e7e6d2af076f12",
     "url": "/static/js/main.63db4f7e.chunk.js"
   },
   {
