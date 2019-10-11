8277f217a1165ec916b3a69bd193cea145962355
lqdsqd <wesley.lebert@gmail.com>
2019-10-11 15:39:19 -0500

    Updates

---
+++ precache-manifest.2eefb93fafb82053ac2b9f4035ebb883.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd81071746cb14b570abb75e77b9d7e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76cd18cd36b065434abf",
+    "url": "/static/css/main.ebcce73d.chunk.css"
+  },
+  {
+    "revision": "16fc980365f0cd392595",
+    "url": "/static/js/2.d8d92bc4.chunk.js"
+  },
+  {
+    "revision": "76cd18cd36b065434abf",
+    "url": "/static/js/main.723f4922.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.557c385435583857b7cb2e292c876eb6.js"
+  "/precache-manifest.2eefb93fafb82053ac2b9f4035ebb883.js"
 );
 
 self.addEventListener('message', (event) => {
